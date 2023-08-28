03ee8b99329ad1833f0a618d507b29e1f498d8f9
8Eithel8 <eithel@list.ru>
2023-08-29 00:35:10 +0300

    feat(#6): сделать компонент секций
    сделан компонент секций

---
+++ src/components/Section.vue
@@ -0,0 +1,62 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: "Документы",
+    },
+    text: {
+        type: String,
+        default: "Документы",
+    },
+    decoration: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<template>
+    <section class="section">
+        <div class="section__decoration" v-show="decoration"></div>
+        <h2 class="section__title">{{ title }}</h2>
+        <p class="section__text">{{ text }}</p>
+        <slot></slot>
+    </section>
+</template>
+
+<style scoped>
+.section {
+    max-width: 1366rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100rem 139rem;
+}
+
+.section__decoration {
+    width: 60rem;
+    height: 48rem;
+    background: url("./icons/decoration.svg") no-repeat center;
+}
+
+.section__title {
+    font-family: var(--font-text);
+    color: var(--c-text);
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36rem;
+    line-height: 48rem;
+    margin: 15rem 0 19rem 0;
+}
+
+.section__text {
+    max-width: 568rem;
+    font-family: var(--font-text);
+    color: var(--c-text);
+    font-weight: 400;
+    font-size: 16rem;
+    line-height: 21rem;
+    margin: 0;
+    text-align: center;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,33 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Promo from '@/components/Promo.vue';
+import Section from '@/components/Section.vue';
 </script>
 
 <template>
   <main>
-<!--    <TheWelcome />-->
+    <Promo/>
+    <Section
+      decoration
+      title="Welcome to Nature"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua."
+    >
+    </Section>
+    <Section
+      title="Proudly presented by"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua."
+    >
+    </Section>
+
+    <Section
+      title="Subscribe to Our Newsletter"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua."
+    >
+    </Section>
+    <Section
+      decoration
+      title="Read Our Blog"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  incididunt ut labore et dolore magna aliqua."
+    >
+    </Section>
   </main>
 </template>
